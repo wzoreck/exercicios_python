5250db1b569a3483a204bd6e75e4830b75325c0b
Daniel Wzoreck <53614252+wzoreck@users.noreply.github.com>
2020-03-29 11:15:18 -0300

    Update and rename ex010.py to ex010_conversao_real_dollar.py

--- ex010_conversao_real_dollar.py
+++ ex010_conversao_real_dollar.py
@@ -1,4 +1,4 @@
-# Crie um programa que leia quanto dinheiro uma pessoa tem na careira
-# e mostre quantos dólarees ela pode comprar
+# Crie um programa que leia quanto dinheiro uma pessoa tem na carteira
+# e mostre quantos dólares ela pode comprar
 reais = float(input('Digite quantos reais você possui: '))
 print('Com R${:.2f} você consegue comprar US${:.2f}'.format(reais,reais/3.7))
