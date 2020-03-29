18a6d4ea7ffb10f7cc17495a3c47ef06c7986b26
Daniel Wzoreck <53614252+wzoreck@users.noreply.github.com>
2020-03-29 11:24:52 -0300

    Update and rename ex020.py to ex020_embaralhando_lista.py

--- ex020_embaralhando_lista.py
+++ ex020_embaralhando_lista.py
@@ -1,5 +1,5 @@
 # O mesmo professor do desafio anterior quer sortear a ordem
-# de apresentação de trabalhos dos alunos. Faça um programa que leia o nomedos quatro
+# de apresentação de trabalhos dos alunos. Faça um programa que leia o nome dos quatro
 # alunos e mostre a ordem sorteada
 from random import choice, shuffle
 a1 = input('Digite o nome do aluno 1: ')
