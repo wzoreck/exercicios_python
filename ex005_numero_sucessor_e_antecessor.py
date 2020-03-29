7cc8a1f03fd78a68399c64ddb3b750ad99c0708e
Daniel Wzoreck <53614252+wzoreck@users.noreply.github.com>
2020-03-29 11:08:58 -0300

    Update and rename ex005.py to ex005_numero_sucessor_e_antecessor.py

--- ex005_numero_sucessor_e_antecessor.py
+++ ex005_numero_sucessor_e_antecessor.py
@@ -1,5 +1,5 @@
 # Faça um programa que leia um número inteiro e mostre na tela
-# seu sucesor e seu antecessor
+# seu sucessor e seu antecessor
 var1 = int(input('Digite um número inteiro: '))
 antecessor = var1 - 1
 sucessor = var1 + 1
