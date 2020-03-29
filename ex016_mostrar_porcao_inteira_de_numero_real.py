c76c0e6054657351e046047f86fe2d6c2cef2028
Daniel Wzoreck <53614252+wzoreck@users.noreply.github.com>
2020-03-29 11:20:34 -0300

    Update and rename ex016.py to ex016_mostrar_porcao_inteira_de_numero_real.py

--- ex016_mostrar_porcao_inteira_de_numero_real.py
+++ ex016_mostrar_porcao_inteira_de_numero_real.py
@@ -1,4 +1,4 @@
-# Crie um programa que leia um número real qualuqer pelo teclado
+# Crie um programa que leia um número real qualquer pelo teclado
 # e mostre na tela a sua porção inteira
 
 '''from math import trunc
