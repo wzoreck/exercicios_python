164bedf2b44ed143215a9c340beee8426fdcd9c6
Daniel Wzoreck <wzoreck2000@gmail.com>
2021-04-07 14:58:49 -0300

    Conteudo sobre getters e setters adicionado

--- POO/Produto.py
+++ POO/Produto.py
@@ -6,3 +6,20 @@ class Produto:
     def desconto(self, percentual):
         self.preco = self.preco - (self.preco * (percentual / 100))
 
+
+    # Com esses decoradores os metodos abaixo são chamados até na passagem de parâmetro do construtor, é uma
+    # diferença existente entre Python e outras linguagens
+
+    # Decorador @property usado para informar um prioridade
+    @property
+    def preco(self):
+        # Para evitar problemas com loop coloca-se um _ antes do nome do atributo
+        return self._preco
+
+    # Criação de metodo setter
+    @preco.setter
+    def preco(self, valor):
+        if isinstance(valor, str):
+            valor = float(valor.replace('R$',''))
+        self._preco = valor
+
--- POO/main.py
+++ POO/main.py
@@ -12,6 +12,7 @@ print(f'O nome é {nome}')
 
 print(f'Tem {Pessoa.getIdade(2000)} anos')
 
+#camiseta = Produto('Camiseta', 'R$50')
 camiseta = Produto('Camiseta', 50)
 camiseta.desconto(10)
 print(f'O preço com desconto fica {camiseta.preco}')
